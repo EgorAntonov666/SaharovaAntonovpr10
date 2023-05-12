ffa581992f305bf65fd7ca43405312d11156ac32
unknown <egorantonov1804@gmail.com>
2023-05-12 21:44:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pr10/presentation/theme/CookBook.kt
@@ -0,0 +1,11 @@
+package com.example.pr10.presentation.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CookBook : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cook_book)
+    }
+}
